4d3731ec5515e7329119c54694f6a5a6d2fbd3dc
Andrew Chen <achen88@mit.edu>
2020-08-03 19:03:40 -0400

    format code & reply

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -5,21 +5,40 @@ import "../styles/content.css";
 
 const ReplySVG = chrome.runtime.getURL('js/' + svgHash);
 
+const replyHeader = "Replied to you:\n"
+const truncateLength = 100;
+
+const format = (replyText) => {
+    if (replyText.indexOf("Quote Tweet") === 0) {
+        const nextNewLineIndex = replyText.indexOf("\n", 12);
+        const lastNewLineIndex = replyText.indexOf("\n", nextNewLineIndex+1);
+        const truncateIndicator = replyText.substring(lastNewLineIndex+1, lastNewLineIndex+1+truncateLength).length < replyText.substring(lastNewLineIndex+1).length ? "..." : "";
+        return replyHeader + "Quote Tweet | " + replyText.substring(12, nextNewLineIndex) + " | " + replyText.substring(nextNewLineIndex+1, lastNewLineIndex) + replyText.substring(lastNewLineIndex+1, lastNewLineIndex+1+truncateLength) + truncateIndicator;
+    } else {
+        const truncateIndicator = replyText.substring(0, truncateLength).length < replyText.length ? "..." : "";
+        return replyHeader + replyText.substring(0, truncateLength) + truncateIndicator;
+    }
+}
+
+const inject = (addedNode) => {
+    const tweetContainer = addedNode.firstChild.firstChild;
+    const tweetSpan = tweetContainer.firstChild.nextSibling.firstChild.firstChild.firstChild as HTMLSpanElement;
+    const buttonMenu = tweetContainer.lastChild;
+    const button = buttonMenu.lastChild as HTMLDivElement;
+    if (button.getAttribute("aria-label") === "More actions") {
+        const container = document.createElement("div");
+        buttonMenu.appendChild(container);
+        ReactDOM.render(Reply({ data: format(tweetSpan.innerText) }), container);
+    }
+}
+
 let observer = new MutationObserver(mutations => {
     for(let mutation of mutations) {
         for(let addedNode of mutation.addedNodes) {
             if (addedNode.nodeName === "DIV") {
+                console.log(addedNode);
                 try {
-                    const tweetContainer = addedNode.firstChild.firstChild;
-                    const tweetSpan = tweetContainer.firstChild.nextSibling.firstChild.firstChild.firstChild as HTMLSpanElement;
-                    const buttonMenu = tweetContainer.lastChild;
-                    const button = buttonMenu.lastChild as HTMLDivElement;
-
-                    if (button.getAttribute("aria-label") === "More actions") {
-                        const container = document.createElement("div");
-                        buttonMenu.appendChild(container);
-                        ReactDOM.render(Reply({ data: tweetSpan.innerText }), container);
-                    }
+                    inject(addedNode);
                 } catch (error) {} // bro i dont give a fuuuuuuck, body that shit
             }
         }
